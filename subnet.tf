c81e0ba06ea49aafc05fde1d5f717303828b8986
rickymanx4 <rickymanx4@naver.com>
2024-04-11 11:49:32 +0900

    subnet #4

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@
 # 1. Public Subnet
 ###
 resource "aws_subnet" "all_subnet" {
-  vpc_id     = aws_vpc.project_vpc.id 
+  vpc_id  = aws_vpc.project_vpc.id 
   for_each = var.subnet
   cidr_block = each.value.cidr
   availability_zone = each.value.az
