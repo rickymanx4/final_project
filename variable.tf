4e687a0ff71654cab22bc2115652dc007438fb3d
rickymanx4 <rickymanx4@naver.com>
2024-04-11 10:38:01 +0900

    65

--- variable.tf
+++ variable.tf
@@ -11,19 +11,19 @@ variable "vpc" {
     {
         name = user_dmz_vpc
         cidr = "10.10.0.0/16"
-    },
+    }
     {
         name = dev_dmz_vpc
         cidr = "10.30.0.0/16"
-    },
+    }
     {
         name = shared_vpc
         cidr = "10.100.0.0/16"
-    },
+    }
     {
         name = product_vpc
         cidr = "10.210.0.0/16"
-    },
+    }
     {
         name = testdev_vpc
         cidr = "10.230.0.0/16"
