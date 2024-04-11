c27d310e960c63976528f21ce90c2362f865c0fa
rickymanx4 <rickymanx4@naver.com>
2024-04-11 22:11:52 +0900

    rt # 7

--- routingtable.tf
+++ routingtable.tf
@@ -69,7 +69,7 @@ resource "aws_route_table" "dev_dmz_pri_rt_c" {
 
 ################################ c. shared_zone ################################
 resource "aws_route_table" "shared_pri_rt" {
-  for_each = length(var.subnet_shared)
+  for_each = var.subnet_shared
   vpc_id = aws_vpc.project_vpc["shared_vpc"].id  
   # route {
   #   cidr_block = "0.0.0.0/0"
@@ -82,7 +82,7 @@ resource "aws_route_table" "shared_pri_rt" {
 
 ################################ d. product_zone ################################
 resource "aws_route_table" "product_pri_rt_a" {
-  for_each = length(var.subnet_product_a) 
+  for_each = var.subnet_product_a
   vpc_id = aws_vpc.project_vpc["product_vpc"].id  
   # route {
   #   cidr_block = "0.0.0.0/0"
@@ -94,7 +94,7 @@ resource "aws_route_table" "product_pri_rt_a" {
 }
 
 resource "aws_route_table" "product_pri_rt_c" {
-  for_each = length(var.subnet_product_c) 
+  for_each = var.subnet_product_c
   vpc_id = aws_vpc.project_vpc["product_vpc"].id  
   # route {
   #   cidr_block = "0.0.0.0/0"
@@ -107,7 +107,7 @@ resource "aws_route_table" "product_pri_rt_c" {
 
 ################################ e. testdev_zone ################################
 resource "aws_route_table" "testdev_pri_rt_a" {
-  for_each = length(var.subnet_testdev_a)
+  for_each = var.subnet_testdev_a
   vpc_id = aws_vpc.project_vpc["testdev_vpc"].id  
   # route {
   #   cidr_block = "0.0.0.0/0"
@@ -119,7 +119,7 @@ resource "aws_route_table" "testdev_pri_rt_a" {
 }
 
 resource "aws_route_table" "testdev_pri_rt_c" {
-  for_each = length(var.subnet_testdev_c)
+  for_each = var.subnet_testdev_c
   vpc_id = aws_vpc.project_vpc["testdev_vpc"].id  
   # route {
   #   cidr_block = "0.0.0.0/0"
