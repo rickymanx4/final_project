0537922c69e6b0460b7ef56bfd42e535a9a02ed0
rickymanx4 <rickymanx4@naver.com>
2024-04-11 10:51:59 +0900

    vb

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,12 +1,22 @@
-# vpcs = [
-#     {
-#         name = user_dmz_vpc
-#         cidr = "10.10.0.0/16"
-#     }
-# ]
-
-#  c = ,
-#     dev_dmz_vpc = "10.30.0.0/16",
-#     shared_vpc = "10.100.0.0/16",
-#     product_vpc = "10.210.0.0/16",
-#     testdev_vpc = "10.230.0.0/16"
+vpc = [
+    {
+        name = "user_dmz_vpc"
+        cidr = "10.10.0.0/16"
+    },
+    {
+        name = "dev_dmz_vpc"
+        cidr = "10.30.0.0/16"
+    },
+    {
+        name = "shared_vpc"
+        cidr = "10.100.0.0/16"
+    },
+    {
+        name = "product_vpc"
+        cidr = "10.210.0.0/16"
+    },
+    {
+        name = "testdev_vpc"
+        cidr = "10.230.0.0/16"
+    }
+]
--- variable.tf
+++ variable.tf
@@ -4,30 +4,34 @@ variable "region" {
   default     = "ap-northeast-3" 
 }
 
-variable "vpc" { 
-  description = "vpc" 
-  type        = map(string)
-  default     = [
-    {
-        name = "user_dmz_vpc"
-        cidr = "10.10.0.0/16"
-    },
-    {
-        name = "dev_dmz_vpc"
-        cidr = "10.30.0.0/16"
-    },
-    {
-        name = "shared_vpc"
-        cidr = "10.100.0.0/16"
-    },
-    {
-        name = "product_vpc"
-        cidr = "10.210.0.0/16"
-    },
-    {
-        name = "testdev_vpc"
-        cidr = "10.230.0.0/16"
-    }
-  ]
+# variable "vpc_name" { 
+#   description = "vpc_name" 
+#   type        = map(string)
+#   default     = [
+#     {
+#         name = "user_dmz_vpc"
+#         cidr = "10.10.0.0/16"
+#     },
+#     {
+#         name = "dev_dmz_vpc"
+#         cidr = "10.30.0.0/16"
+#     },
+#     {
+#         name = "shared_vpc"
+#         cidr = "10.100.0.0/16"
+#     },
+#     {
+#         name = "product_vpc"
+#         cidr = "10.210.0.0/16"
+#     },
+#     {
+#         name = "testdev_vpc"
+#         cidr = "10.230.0.0/16"
+#     }
+#   ]
+# }
+
+variable "vpc" {
+  type = list(any)
 }
 
