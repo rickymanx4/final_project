2b6cb718703b693e41fd5184a6c1999f50f1f507
rickymanx4 <rickymanx4@naver.com>
2024-04-11 11:16:22 +0900

    nb

--- variable.tf
+++ variable.tf
@@ -11,23 +11,23 @@ variable "vpc" {
     cidr  = string
   }))
   default     = {
-    {
+    vpc1 = {
         name = "user_dmz_vpc"
         cidr = "10.10.0.0/16"
-    },
-    {
+    }
+    vpc2 = {
         name = "dev_dmz_vpc"
         cidr = "10.30.0.0/16"
-    },
-    {
+    }
+    vpc3 = {
         name = "shared_vpc"
         cidr = "10.100.0.0/16"
-    },
-    {
+    }
+    vpc4 = {
         name = "product_vpc"
         cidr = "10.210.0.0/16"
-    },
-    {
+    }
+    vpc5 = {
         name = "testdev_vpc"
         cidr = "10.230.0.0/16"
     }
