bc6aa9e448bce3e03539a39ac6c70819c48abba9
rickymanx4 <rickymanx4@naver.com>
2024-04-11 11:31:12 +0900

    6

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "project_vpc" {
     for_each = var.vpc 
     cidr_block = each.value.cidr
     tags = { 
-        name = each.value.name
+        Name = each.value.name
     }
     enable_dns_hostnames      = true
     enable_dns_support        = true
