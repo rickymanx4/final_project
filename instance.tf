54a21caf9c8f145d7893bf5d6db6341ca8e699a3
rickymanx4 <rickymanx4@naver.com>
2024-04-12 14:48:53 +0900

    ec2 # 1

--- instance.tf
+++ instance.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "dev_dmz_proxy" {
 resource "aws_instance" "shared_nexus" {
   ami = data.aws_ami.amazon_linux_2023.id
   instance_type = "t2.small" 
-  vpc_security_group_ids = [aws_security_group.security_group.shared_nexus_sg.id]
+  vpc_security_group_ids = [aws_security_group.shared_nexus_sg.id]
   key_name = aws_key_pair.ec2_key.key_name
   subnet_id = aws_subnet.shared_subnet["shared_pri_01a"].id
   associate_public_ip_address = false
@@ -70,7 +70,7 @@ resource "aws_instance" "shared_monitoring" {
   count = length(var.monitoring_ec2)
   ami = data.aws_ami.amazon_linux_2023.id
   instance_type = "t2.small" 
-  vpc_security_group_ids = [aws_security_group.security_group.shared_monitoring_sg.id]
+  vpc_security_group_ids = [aws_security_group.shared_monitoring_sg.id]
   key_name = aws_key_pair.ec2_key.key_name
   subnet_id = aws_subnet.shared_subnet["shared_pri_02a"].id
   associate_public_ip_address = false
@@ -92,7 +92,7 @@ resource "aws_instance" "shared_monitoring" {
 resource "aws_instance" "shared_elk" {  
   ami = data.aws_ami.amazon_linux_2023.id
   instance_type = "t2.small" 
-  vpc_security_group_ids = [aws_security_group.security_group.shared_elk_sg.id]
+  vpc_security_group_ids = [aws_security_group.shared_elk_sg.id]
   key_name = aws_key_pair.ec2_key.key_name
   subnet_id = aws_subnet.shared_subnet["shared_pri_02a"].id
   associate_public_ip_address = false
