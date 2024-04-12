8b146a8dae03b2457714bdc84a6c68eea35a9165
rickymanx4 <rickymanx4@naver.com>
2024-04-12 10:15:37 +0900

    sg # 4

--- security-group.tf
+++ security-group.tf
@@ -1,35 +1,6 @@
 ###
 # 1. Instance
 ### 
-resource "aws_security_group" "shared_nexus_sg" { 
-  name = "shared_nexus_sg" 
-  description = "Security Group for shared_nexus" 
-  vpc_id = aws_vpc.project_vpc["shared_vpc"].id
-
-  ingress {
-  from_port     = 22
-  to_port       = 22
-  protocol      = "tcp"
-  cidr_blocks   = ["0.0.0.0/0"]
-  }
-
-  ingress {
-  from_port     = 8080
-  to_port       = 8080
-  protocol      = "tcp"
-  cidr_blocks   = ["0.0.0.0/0"]
-  }
-
-  egress {
-  from_port     = 0
-  to_port       = 0
-  protocol      = "-1"
-  cidr_blocks   = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "shared_nexus_sg"
-  }
-}
 
 resource "aws_security_group" "user_dmz_proxy_sg" {
   name = "user_dmz_proxy_sg" 
@@ -37,8 +8,8 @@ resource "aws_security_group" "user_dmz_proxy_sg" {
   vpc_id = aws_vpc.project_vpc["user_dmz_vpc"].id
 
   ingress {
-  from_port     = 8080
-  to_port       = 8080
+  from_port     = 80
+  to_port       = 80
   protocol      = "tcp"
   security_groups   = [aws_security_group.user_dmz_elb_sg.id]
   }
@@ -60,12 +31,19 @@ resource "aws_security_group" "user_dmz_elb_sg" {
   vpc_id = aws_vpc.project_vpc["user_dmz_vpc"].id
 
   ingress {
-  from_port     = 8080
-  to_port       = 8080
+  from_port     = 80
+  to_port       = 80
   protocol      = "tcp"
   cidr_blocks   = ["0.0.0.0/0"]
   }
-  
+
+  ingress {
+  from_port     = 9999
+  to_port       = 9999
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
   egress {
   from_port     = 0
   to_port       = 0
@@ -77,173 +55,244 @@ resource "aws_security_group" "user_dmz_elb_sg" {
   }
 }
 
-# resource "aws_security_group" "project_app" {
-#   name = "project_app" 
-#   description = "Security Group for APP Layer Instance" 
-#   vpc_id = aws_vpc.project_vpc.id
+resource "aws_security_group" "dev_dmz_proxy_sg" {
+  name = "dev_dmz_proxy_sg" 
+  description = "Security Group for ngninx_proxy_instance in dev_dmz" 
+  vpc_id = aws_vpc.project_vpc["dev_dmz_vpc"].id
 
-#   ingress {
-#   from_port     = 22
-#   to_port       = 22
-#   protocol      = "tcp"
-#   security_groups   = [aws_security_group.project_bastion.id]
-#   }
-#   ingress {
-#   from_port     = 9100
-#   to_port       = 9100
-#   protocol      = "tcp"
-#   security_groups   = [aws_security_group.project_bastion.id]
-#   }
-#   ingress {
-#     description = "flask service"
-#     from_port     = 5000 # 필요에 따라 수정 가능
-#     to_port       = 5000 # 필요에 따라 수정 가능
-#     protocol      = "tcp"
-#     security_groups   = [aws_security_group.project_int-lb.id]
-#   }
+  ingress {
+  from_port     = 80
+  to_port       = 80
+  protocol      = "tcp"
+  security_groups   = [aws_security_group.dev_dmz_elb_sg.id]
+  }
   
-#   egress {
-#   from_port     = 0
-#   to_port       = 0
-#   protocol      = "-1"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "project_APP"
-#   }
-# }
-# ###
-# # 2. Load-Balancer
-# ###
-# resource "aws_security_group" "project_ext-lb" {
-#   name = "project_ext-lb" 
-#   description = "Security Group for External Load Balancer" 
-#   vpc_id = aws_vpc.project_vpc.id
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "dev_dmz_proxy_sg"
+  }
+}
 
-#   ingress {
-#   from_port     = 80
-#   to_port       = 80
-#   protocol      = "tcp"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
+resource "aws_security_group" "dev_dmz_elb_sg" {
+  name = "dev_dmz_elb_sg" 
+  description = "Security Group for load_balancer in dev_dmz" 
+  vpc_id = aws_vpc.project_vpc["dev_dmz_vpc"].id
 
-#   egress {
-#   from_port     = 0
-#   to_port       = 0
-#   protocol      = "-1"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "project_Ext-LB"
-#   }
-# }
-
-# resource "aws_security_group" "project_int-lb" {
-#   name = "project_int-lb" 
-#   description = "Security Group for Internal Load Balancer" 
-#   vpc_id = aws_vpc.project_vpc.id
-
-#   ingress {
-#   from_port     = 80 # 필요에 따라 수정 가능
-#   to_port       = 80 # 필요에 따라 수정 가능
-#   protocol      = "tcp"
-#   security_groups = [aws_security_group.project_web.id]
-#   }
-#   ingress {
-#     description = "flask service"
-#     from_port     = 5000 # 필요에 따라 수정 가능
-#     to_port       = 5000 # 필요에 따라 수정 가능
-#     protocol      = "tcp"
-#     security_groups   = [aws_security_group.project_web.id]
-#   }
+  ingress {
+  from_port     = 80
+  to_port       = 80
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  ingress {
+  from_port     = 8888
+  to_port       = 8888
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }  
   
-#   egress {
-#   from_port     = 0
-#   to_port       = 0
-#   protocol      = "-1"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "project_Int-LB"
-#   }
-# }
-# ###
-# # 3. DB
-# ###
-# resource "aws_security_group" "project_db" {
-#   name = "project_db" 
-#   description = "Security Group for RDS DB" 
-#   vpc_id = aws_vpc.project_vpc.id
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "dev_dmz_elb_sg"
+  }
+}
 
-#   ingress {
-#   from_port     = 3306 
-#   to_port       = 3306
-#   protocol      = "tcp"
-#   security_groups   = [aws_security_group.project_web.id]
-#   }
-#   ingress {
-#   from_port     = 3306
-#   to_port       = 3306
-#   protocol      = "tcp"
-#   security_groups   = [aws_security_group.project_app.id]
-#   }
-  
-#   egress {
-#   from_port     = 0
-#   to_port       = 0
-#   protocol      = "-1"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "project_DB"
-#   }
-# }
 
-# ###
-# # 4. EFS
-# ###
-# resource "aws_security_group" "project_web_efs" {
-#   name = "project_web_efs" 
-#   description = "Security Group for Web EFS" 
-#   vpc_id = aws_vpc.project_vpc.id
 
-#   ingress {
-#   from_port     = 2049
-#   to_port       = 2049
-#   protocol      = "tcp"
-#   security_groups   = [aws_security_group.project_web.id]
-#   }
 
-#   egress {
-#   from_port     = 0
-#   to_port       = 0
-#   protocol      = "-1"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "project_Web_efs"
-#   }
-# }
+resource "aws_security_group" "shared_nexus_sg" { 
+  name = "shared_nexus_sg" 
+  description = "Security Group for shared_nexus" 
+  vpc_id = aws_vpc.project_vpc["shared_vpc"].id
 
-# resource "aws_security_group" "project_app_efs" {
-#   name = "project_app_efs" 
-#   description = "Security Group for APP EFS" 
-#   vpc_id = aws_vpc.project_vpc.id
+  ingress {
+  from_port     = 22
+  to_port       = 22
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#   from_port     = 2049
-#   to_port       = 2049
-#   protocol      = "tcp"
-#   security_groups   = [aws_security_group.project_app.id]
-#   }
-  
-#   egress {
-#   from_port     = 0
-#   to_port       = 0
-#   protocol      = "-1"
-#   cidr_blocks   = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "project_APP_efs"
-#   }
-# }
+  ingress {
+  from_port     = 80
+  to_port       = 80
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "shared_nexus_sg"
+  }
+}
+
+resource "aws_security_group" "shared_monitoring_sg" { 
+  name = "shared_monitoring_sg" 
+  description = "Security Group for shared_monitoring" 
+  vpc_id = aws_vpc.project_vpc["shared_vpc"].id
+
+  ingress {
+  from_port     = 9090
+  to_port       = 9090
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port     = 3000
+  to_port       = 3000
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "shared_monitoring_sg"
+  }
+}
+
+resource "aws_security_group" "shared_elk_sg" { 
+  name = "shared_elk_sg" 
+  description = "Security Group for shared_elk" 
+  vpc_id = aws_vpc.project_vpc["shared_vpc"].id
+
+  ingress {
+  from_port     = 5044
+  to_port       = 5044
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port     = 5601
+  to_port       = 5601
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port     = 9000
+  to_port       = 9000
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "shared_elk_sg"
+  }
+}
+resource "aws_security_group" "shared_eks_sg" { 
+  name = "shared_eks_sg" 
+  description = "Security Group for shared_eks" 
+  vpc_id = aws_vpc.project_vpc["shared_vpc"].id
+
+  ingress {
+  from_port     = 80
+  to_port       = 80
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port     = 6000
+  to_port       = 6000
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "shared_eks_sg"
+  }
+}
+
+
+
+resource "aws_security_group" "product_eks_sg" { 
+  name = "product_eks_sg" 
+  description = "Security Group for product_eks" 
+  vpc_id = aws_vpc.project_vpc["product_vpc"].id
+
+  ingress {
+  from_port     = 80
+  to_port       = 80
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port     = 6000
+  to_port       = 6000
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "product_eks_sg"
+  }
+}
+
+resource "aws_security_group" "testdev_eks_sg" { 
+  name = "testdev_eks_sg" 
+  description = "Security Group for testdev_eks" 
+  vpc_id = aws_vpc.project_vpc["testdev_vpc"].id
+
+  ingress {
+  from_port     = 80
+  to_port       = 80
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  from_port     = 6000
+  to_port       = 6000
+  protocol      = "tcp"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+
+  egress {
+  from_port     = 0
+  to_port       = 0
+  protocol      = "-1"
+  cidr_blocks   = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "product_eks_sg"
+  }
+}
