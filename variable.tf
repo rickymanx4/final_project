bbfedada202f047fa714a4d9c729e76368451088
rickymanx4 <rickymanx4@naver.com>
2024-04-13 22:00:56 +0900

    subnet # 5

--- variable.tf
+++ variable.tf
@@ -4,36 +4,6 @@ variable "region" {
   default     = "ap-southeast-1" 
 }
 
-# variable "vpc" { 
-#   description = "vpc" 
-#   type        = map(object({
-#     name  = string
-#     cidr  = string
-#   }))
-#   default     = {
-#     user_dmz_vpc = {
-#         name = "user_dmz_vpc"
-#         cidr = "10.10.0.0/16"
-#     }
-#     dev_dmz_vpc = {
-#         name = "dev_dmz_vpc"
-#         cidr = "10.30.0.0/16"
-#     }
-#     shared_vpc = {
-#         name = "shared_vpc"
-#         cidr = "10.100.0.0/16"
-#     }
-#     product_vpc = {
-#         name = "product_vpc"
-#         cidr = "10.210.0.0/16"
-#     }
-#     testdev_vpc = {
-#         name = "testdev_vpc"
-#         cidr = "10.230.0.0/16"
-#     }
-#   }
-# }
-
 variable "vpc" {
   type        = list(string)
   description = "vpc"
