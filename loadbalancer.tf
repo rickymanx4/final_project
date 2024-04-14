80409068dddfaa27b9a74b4b5a373c0f9374321f
rickymanx4 <rickymanx4@naver.com>
2024-04-14 18:39:33 +0900

    lb # 5

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,6 +1,8 @@
-###
-# 1. External-Loadb-Balancer
-###
+##############################################################################
+#################################### 1.user_dmz_lb ###########################
+##############################################################################
+
+######################### a. target_groups ####################################
 
 resource "aws_lb_target_group" "user_dmz_proxy_tg" {
   count       = 2
@@ -26,12 +28,7 @@ resource "aws_lb_target_group" "user_dmz_nexus_tg" {
   vpc_id = local.user_dev_vpc[0]
 }
 
-# resource "aws_lb_target_group_attachment" "user_dmz_to_nexus_att" {
-#   count            = 2
-#   target_group_arn = aws_lb_target_group.user_dmz_nexus_tg[count.index].arn
-#   target_id        = aws_instance.user_dmz_proxy[count.index].id
-#   port = 80
-# }
+######################### b. load_balancer ####################################
 
 resource "aws_lb" "user_dmz_proxy_lb" {
   count              = 2
@@ -64,6 +61,12 @@ resource "aws_lb_listener" "user_nexus_lb_listener" {
   }
 }
 
+##############################################################################
+#################################### 2.dev_dmz_lb ###########################
+##############################################################################
+
+######################### a. target_groups ####################################
+
 resource "aws_lb_target_group" "dev_dmz_proxy_tg" {
   count       = 2
   name        = "dev-dmz-target-group-${local.az_ac[count.index]}"
@@ -88,13 +91,7 @@ resource "aws_lb_target_group" "dev_dmz_nexus_tg" {
   vpc_id = local.user_dev_vpc[1]
 }
 
-# resource "aws_lb_target_group_attachment" "dev_dmz_to_nexus_att" {
-#   count            = 2
-#   target_group_arn = aws_lb_target_group.dev_dmz_nexus_tg[count.index].arn
-#   target_id        = aws_instance.dev_dmz_proxy[count.index].id
-#   port = 80
-# }
-
+######################### b. load_balancer ####################################
 
 resource "aws_lb" "dev_dmz_proxy_lb" {
   count              = 2
@@ -127,7 +124,11 @@ resource "aws_lb_listener" "dev_nexus_lb_listener" {
   }
 }
 
-################################################################################
+##############################################################################
+#################################### 3.shared_lb #############################
+##############################################################################
+
+######################### a. target_groups ####################################
 resource "aws_lb_target_group" "nexus_tg" {
   name        = "shared-nexus-ext-lb-tg"
   port        = 22
@@ -140,6 +141,29 @@ resource "aws_lb_target_group_attachment" "nexus_tg_att" {
     target_id = aws_instance.shared_nexus.id
     port = 22
 }
+
+resource "aws_lb_target_group" "shared_int_tg" {
+  count       = length(local.shared_ec2_name)
+  name        = "shared-${local.shared_ec2_name[count.index]}-tg"
+  port        = 22
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.project_vpc[2].id
+}
+
+resource "aws_lb_target_group_attachment" "shared_monitoring_att" {
+    count            = 2 
+    target_group_arn = aws_lb_target_group.shared_int_tg[count.index].arn
+    target_id        = aws_instance.shared_monitoring[count.index].id
+    port             = 22 
+}
+
+resource "aws_lb_target_group_attachment" "shared_elk_att" { 
+    target_group_arn = aws_lb_target_group.shared_int_tg[2].arn
+    target_id        = aws_instance.shared_elk.id
+    port             = 22 
+}
+
+######################### b. load_balancer ####################################
 
 resource "aws_lb" "shared_ext_lb" {
   name                = "shared-ext-lb"
@@ -163,27 +187,6 @@ resource "aws_lb_listener" "nexus_listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.nexus_tg.arn
   }
-}
-
-resource "aws_lb_target_group" "shared_int_tg" {
-  count       = length(local.shared_ec2_name)
-  name        = local.shared_ec2_name[count.index]
-  port        = 22
-  protocol    = "TCP"
-  vpc_id      = aws_vpc.project_vpc[2].id
-}
-
-resource "aws_lb_target_group_attachment" "shared_monitoring_att" {
-    count            = 2 
-    target_group_arn = aws_lb_target_group.shared_int_tg[count.index].arn
-    target_id        = aws_instance.shared_monitoring[count.index].id
-    port             = 22 
-}
-
-resource "aws_lb_target_group_attachment" "shared_elk_att" { 
-    target_group_arn = aws_lb_target_group.shared_int_tg[2].arn
-    target_id        = aws_instance.shared_elk.id
-    port             = 22 
 }
 
 resource "aws_lb" "shared_int" {
@@ -223,3 +226,4 @@ resource "aws_lb_listener" "shared_int_listen02" {
     target_group_arn = aws_lb_target_group.shared_int_tg[2].arn
   }
 }  
+
