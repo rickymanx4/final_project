13b920ae45a7d5b335bdd8d18a36cd178076649a
rickymanx4 <rickymanx4@naver.com>
2024-04-14 14:08:27 +0900

    lb #4

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -19,11 +19,11 @@ resource "aws_lb_target_group_attachment" "user_dmz_proxy_tg_att" {
 
 resource "aws_lb" "user_dmz_proxy_lb" {
   count              = 2
-  name               = "${local.names[count.index]}-proxy-lb"
+  name               = "${local.names[0]}-proxy-lb"
   load_balancer_type = "network"
   internal = false
   subnets = aws_subnet.user_dmz_pri_subnet[count.index]
-  security_groups = [aws_security_group.user_dmz_proxy_sg[0].id]
+  security_groups = [aws_security_group.dmz_elb_sg[0].id]
 }
 resource "aws_lb_listener" "user_proxy_lb_listener" {
   count             = 2
@@ -36,51 +36,39 @@ resource "aws_lb_listener" "user_proxy_lb_listener" {
   }
 }
 
-# resource "aws_lb_target_group" "dev_dmz_proxy_tg" {
-#   count       = 2
-#   name        = "${local.names[1]}-target-group-${local.az_ac[count.index]}"
-#   port        = 80
-#   protocol    = "TCP"
-#   target_type = "instance"
-#   vpc_id = aws_vpc.project_vpc[1].id
-# }
-# resource "aws_lb_target_group_attachment" "dev_dmz_proxy_tg_att" {
-#   count            = 2
-#   target_group_arn = aws_lb_target_group.dev_dmz_proxy_tg[count.index].arn
-#   target_id        = aws_instance.dev_dmz_proxy[count.index].id
-#   port = 80
-# }
+resource "aws_lb_target_group" "dev_dmz_proxy_tg" {
+  count       = 2
+  name        = "${local.names[1]}-target-group-${local.az_ac[count.index]}"
+  port        = 80
+  protocol    = "TCP"
+  target_type = "instance"
+  vpc_id = aws_vpc.project_vpc[1].id
+}
+resource "aws_lb_target_group_attachment" "dev_dmz_proxy_tg_att" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.dev_dmz_proxy_tg[count.index].arn
+  target_id        = aws_instance.dev_dmz_proxy[count.index].id
+  port = 80
+}
 
-# resource "aws_lb" "dev_dmz_proxy_lb" {
-#   name               = "ext-lb"
-#   load_balancer_type = "network"
-#   internal = false
-#   subnets = aws_subnet.public[*].id
-#   security_groups = [aws_security_group.project_ext-lb.id]
-# }
-# resource "aws_lb_listener" "ext_listener" {
-#   load_balancer_arn = aws_alb.external_lb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.ext-tg.arn
-#   }
-# }
-
-
-# resource "aws_lb" "test" {
-#   name               = "test-lb-tf"
-#   internal           = false
-#   load_balancer_type = "network"
-#   subnets            = [for subnet in aws_subnet.public : subnet.id]
-
-#   enable_deletion_protection = true
-
-#   tags = {
-#     Environment = "production"
-#   }
-# }
+resource "aws_lb" "dev_dmz_proxy_lb" {
+  count              = 2
+  name               = "${local.names[1]}-proxy-lb"
+  load_balancer_type = "network"
+  internal = false
+  subnets = aws_subnet.dev_dmz_pri_subnet[count.index]
+  security_groups = [aws_security_group.dmz_elb_sg[0].id]
+}
+resource "aws_lb_listener" "user_proxy_lb_listener" {
+  count             = 2
+  load_balancer_arn = aws_lb.dev_dmz_proxy_lb[count.index].arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dev_dmz_proxy_tg[count.index].arn
+  }
+}
 
 # ###
 # # 2. internal-Loadb-Balancer
