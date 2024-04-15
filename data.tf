f6f248f0aff0b32e7eabb5300bce0f0f0b0c3997
rickymanx4 <rickymanx4@naver.com>
2024-04-15 22:29:56 +0900

    data # 5

--- data.tf
+++ data.tf
@@ -27,7 +27,7 @@ count = 2
 
   filter {
     name   = "description"
-    values = ["ELB net/${aws_lb.shared_ext_lb.name}/*"]
+    values = ["ELB net/${aws_lb.shared_ext_lb[count.index].name}/*"]
   }
   filter {
     name   = "subnet-id"
