13152d6592e3c68058033eea9c4ac8262a079a1c
rickymanx4 <rickymanx4@naver.com>
2024-04-15 22:23:59 +0900

    data # 5

--- data.tf
+++ data.tf
@@ -27,7 +27,7 @@ count = 2
 
   filter {
     name   = "description"
-    values = "ELB ${aws_lb.shared_ext_lb[count.index]}"
+    values = "ELB ${aws_lb.shared_ext_lb}"
   }
   filter {
     name   = "subnet-id"
