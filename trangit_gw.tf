6ab7654cb061e6e4bf3a3064fa415e049416e4b3
rickymanx4 <rickymanx4@naver.com>
2024-04-16 00:33:57 +0900

    tgw # 6

--- trangit_gw.tf
+++ trangit_gw.tf
@@ -152,8 +152,8 @@ resource "aws_ec2_transit_gateway_route_table_association" "shared-product-assoc
   transit_gateway_route_table_id  = aws_ec2_transit_gateway_route_table.tgw_rt[2].id
 }
 
-resource "aws_ec2_transit_gateway_route_table_association" "shared-product-assoc" {
-  transit_gateway_attachment_id   = aws_ec2_transit_gateway_vpc_attachment.product.id
+resource "aws_ec2_transit_gateway_route_table_association" "shared-testdev-assoc" {
+  transit_gateway_attachment_id   = aws_ec2_transit_gateway_vpc_attachment.testdev.id
   transit_gateway_route_table_id  = aws_ec2_transit_gateway_route_table.tgw_rt[2].id
 }
 
