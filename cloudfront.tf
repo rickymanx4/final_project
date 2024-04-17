177a36022ba668ff454b66738abec6450d997026
rickymanx4 <rickymanx4@naver.com>
2024-04-18 00:18:21 +0900

    cf # 1

--- cloudfront.tf
+++ cloudfront.tf
@@ -54,7 +54,7 @@ resource "aws_cloudfront_distribution" "alb_beanstalk" {
     viewer_protocol_policy = "allow-all"
     min_ttl                = 0
     default_ttl            = 3600
-    max_ttl
+    max_ttl                = 86400
     }
     viewer_certificate {
     cloudfront_default_certificate = true
