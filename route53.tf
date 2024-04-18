81bbe0c7710e647b2df56fba999aecbc700b2411
rickymanx4 <rickymanx4@naver.com>
2024-04-19 01:11:29 +0900

    rt 53 # 1

--- route53.tf
+++ route53.tf
@@ -4,16 +4,16 @@ resource "aws_route53_record" "www-nadri" {
   name           = "www"
   type           = "A"
   set_identifier = "nadri-${local.az_ac[count.index]}"
-  #records        = [aws_lb.user_dmz_proxy_lb[count.index].dns_name]
-  #ttl            = 7200
-  weighted_routing_policy {
-    weight = local.weight[count.index]
-  }
-  alias {
-    name                   = aws_lb.user_dmz_proxy_lb.dns_name
-    zone_id                = aws_lb.user_dmz_proxy_lb.zone_id
-    evaluate_target_health = true
-  }
+  records        = [aws_lb.user_dmz_proxy_lb.dns_name]
+  ttl            = 7200
+  # weighted_routing_policy {
+  #   weight = local.weight[count.index]
+  # }
+  # alias {
+  #   name                   = aws_lb.user_dmz_proxy_lb.dns_name
+  #   zone_id                = aws_lb.user_dmz_proxy_lb.zone_id
+  #   evaluate_target_health = true
+  # }
   
 }
 
