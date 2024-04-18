b59249964612f71f5492740227e6fcbec2f72df5
rickymanx4 <rickymanx4@naver.com>
2024-04-18 22:09:24 +0900

    rt53# 5

--- route53.tf
+++ route53.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "www-nadri" {
   name           = "www"
   type           = "A"
   set_identifier = "nadri-${local.az_ac[count.index]}"
-  records        = aws_lb.user_dmz_proxy_lb[count.index].dns_name
+  records        = [aws_lb.user_dmz_proxy_lb[count.index].dns_name]
   ttl            = 7200
   weighted_routing_policy {
     weight = local.weight[count.index]
