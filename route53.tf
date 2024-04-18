2ef2ed193ddfc8d458378533b70c47762b97c42b
rickymanx4 <rickymanx4@naver.com>
2024-04-19 01:36:35 +0900

    route 53 # 5

--- route53.tf
+++ route53.tf
@@ -15,6 +15,23 @@ resource "aws_route53_record" "www-nadri" {
   }  
 }
 
+resource "aws_route53_record" "acm_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = local.host_zone
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name       = "nadri-project.com"
   validation_method = "DNS"
@@ -28,7 +45,9 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+
+
 resource "aws_acm_certificate_validation" "cert_vali" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_vali : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.acm_record : record.fqdn]
 }
