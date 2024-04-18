23ea107670fd02c3037c5bf13779246056ec77de
rickymanx4 <rickymanx4@naver.com>
2024-04-19 01:29:49 +0900

    route 53 # 1

--- route53.tf
+++ route53.tf
@@ -12,7 +12,23 @@ resource "aws_route53_record" "www-nadri" {
     name                   = aws_lb.user_dmz_proxy_lb.dns_name
     zone_id                = aws_lb.user_dmz_proxy_lb.zone_id
     evaluate_target_health = true
-  }
-  
+  }  
 }
 
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "nadri-project.com"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "nadri-cst"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "cert_vali" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_vali : record.fqdn]
+}
