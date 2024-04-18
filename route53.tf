d7b1cf9c15a4d0bfb8f54aba70e2eb1b302996ae
rickymanx4 <rickymanx4@naver.com>
2024-04-18 17:33:51 +0900

    rt53 # 5

--- route53.tf
+++ route53.tf
@@ -9,7 +9,7 @@ resource "aws_route53_record" "www-nadri" {
     weight = local.weight[count.index]
   }
   alias {
-    name                   = aws_elb.user_dmz_proxy_lb[count.index].dns_name
+    name                   = aws_lb.user_dmz_proxy_lb[count.index].dns_name
     zone_id                = local.host_zone
     evaluate_target_health = true
   }
