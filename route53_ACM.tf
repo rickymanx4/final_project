655e30292869982955a325a31c1cfd18ec5fa6b4
rickymanx4 <rickymanx4@naver.com>
2024-04-22 12:17:20 +0900

    route 53 # remove example

--- route53_ACM.tf
+++ route53_ACM.tf
@@ -54,23 +54,23 @@ resource "aws_route53_record" "no_acm_record" {
   depends_on = [ aws_acm_certificate.cert ]
 }
 
-resource "aws_route53_record" "example" {
-  for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
-      name    = dvo.resource_record_name
-      record  = dvo.resource_record_value
-      type    = dvo.resource_record_type
-      zone_id = dvo.domain_name == "example.org" ? data.aws_route53_zone.example_org.zone_id : data.aws_route53_zone.example_com.zone_id
-    }
-  }
+# resource "aws_route53_record" "example" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+#       name    = dvo.resource_record_name
+#       record  = dvo.resource_record_value
+#       type    = dvo.resource_record_type
+#       zone_id = dvo.domain_name == "example.org" ? data.aws_route53_zone.example_org.zone_id : data.aws_route53_zone.example_com.zone_id
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = each.value.zone_id
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = each.value.zone_id
+# }
 
 ######################### b. ACM create ################################
 resource "aws_acm_certificate" "cert" {
