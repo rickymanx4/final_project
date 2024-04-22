b87b67c3bc7c6b3204e693e547001d1c4df2078a
rickymanx4 <rickymanx4@naver.com>
2024-04-22 12:16:50 +0900

    route53 # remove vlidation

--- route53_ACM.tf
+++ route53_ACM.tf
@@ -54,6 +54,24 @@ resource "aws_route53_record" "no_acm_record" {
   depends_on = [ aws_acm_certificate.cert ]
 }
 
+resource "aws_route53_record" "example" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = dvo.domain_name == "example.org" ? data.aws_route53_zone.example_org.zone_id : data.aws_route53_zone.example_com.zone_id
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
+
 ######################### b. ACM create ################################
 resource "aws_acm_certificate" "cert" {
   domain_name                = local.domain_name
@@ -69,8 +87,8 @@ resource "aws_acm_certificate" "cert" {
 }
 
 ######################### c. ACM to route53 validation ###########################
-resource "aws_acm_certificate_validation" "cert_vali" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.no_acm_record : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert_vali" {
+#   certificate_arn         = aws_acm_certificate.cert.arn
+#   validation_record_fqdns = [for record in aws_route53_record.no_acm_record : record.fqdn]
+# }
 
