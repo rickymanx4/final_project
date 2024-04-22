a3bfb8e57df521945f864101fdf4f25111e5d11c
rickymanx4 <rickymanx4@naver.com>
2024-04-22 17:16:12 +0900

    waf # 3 uppercase

--- waf.tf
+++ waf.tf
@@ -50,7 +50,7 @@ resource "aws_wafv2_rule_group" "web_acl_rule_group" {
             arn = aws_wafv2_regex_pattern_set.iphone.arn
             field_to_match {
               single_header {
-                name = "User-Agent"
+                name = "user-agent"
               }
             }
             text_transformation {
