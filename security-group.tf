36c4f4ea62a914e977f263decb8ad27afa9d8455
rickymanx4 <rickymanx4@naver.com>
2024-04-25 14:16:30 +0900

    sg # proxy_sg

--- security-group.tf
+++ security-group.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "dmz_proxy_sg" {
   from_port     = local.dmz_ports[0]
   to_port       = local.dmz_ports[0]
   protocol      = "tcp"
-  security_groups =["10.100.0.0/16"]
+  cidr_blocks   = ["10.100.0.0/16"]
   }
 
   ingress {
@@ -54,12 +54,11 @@ resource "aws_security_group" "dmz_lb_sg" {
 }
 
 resource "aws_security_group_rule" "user_dmz_lb_SG" {
-  count                    = 3
   security_group_id        = aws_security_group.dmz_lb_sg[1].id
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = local.dmz_ports[count.index +1]
-  to_port                  = local.dmz_ports[count.index +1]
+  from_port                = local.dmz_ports[1]
+  to_port                  = local.dmz_ports[1]
   cidr_blocks              = ["0.0.0.0/0"]
 }  
 resource "aws_security_group_rule" "dev_dmz_lb_SG" {
