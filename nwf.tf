6f47dbaf3f031f2d4d3ee36d775d4982ee002b73
rickymanx4 <rickymanx4@naver.com>
2024-04-26 11:11:15 +0900

    nwf # first

--- nwf.tf
+++ nwf.tf
@@ -62,17 +62,17 @@ resource "aws_networkfirewall_firewall_policy" "nwf_policy" {
 
 
 resource "aws_networkfirewall_firewall" "user_network_firewall" { 
-  name                               = "${vars.name[0]-nwf}"
+  name                               = "${var.name[0]-nwf}"
   firewall_policy_arn                = aws_networkfirewall_firewall_policy.nwf_policy.arn
   vpc_id                             = aws_vpc.project_vpc[0]
   # 나중에 true로 변경
   firewall_policy_change_protection  = false
   subnet_change_protection           = false
   subnet_mapping {
-    subnet_id = aws_subnet.user_dmz_pub_subnet[count.index + 2].id
+    subnet_id = aws_subnet.subnet_user_dmz_pub[count.index + 2].id
   }
   tags = {
-    Name = "${vars.name[0]-nwf}" 
+    Name = "${var.name[0]-nwf}" 
   }
   timeouts {
     create = "20m"
@@ -82,17 +82,17 @@ resource "aws_networkfirewall_firewall" "user_network_firewall" {
 }
 
 resource "aws_networkfirewall_firewall" "dev_network_firewall" { 
-  name                               = "${vars.name[1]-nwf}"
+  name                               = "${var.name[1]-nwf}"
   firewall_policy_arn                = aws_networkfirewall_firewall_policy.nwf_policy.arn
   vpc_id                             = aws_vpc.project_vpc[1]
   # 나중에 true로 변경
   firewall_policy_change_protection  = false
   subnet_change_protection           = false
   subnet_mapping {
-    subnet_id = aws_subnet.dev_dmz_pub_subnet[count.index + 2].id
+    subnet_id = aws_subnet.subnet_dev_dmz_pub[count.index + 2].id
   }
   tags = {
-    Name = "${vars.name[1]-nwf}" 
+    Name = "${var.name[1]-nwf}" 
   }
   timeouts {
     create = "20m"
