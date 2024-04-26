281a29becb4d81a8b95fc4682ad7aff33439b86c
rickymanx4 <152831608+rickymanx4@users.noreply.github.com>
2024-04-26 17:41:55 +0900

    nwf

--- nwf.tf
+++ nwf.tf
@@ -3,15 +3,7 @@ resource "aws_networkfirewall_rule_group" "nwf_rule_group" {
   name     = "nwf-rule-group"
   type     = "STATELESS"
   rule_group {
-    rules_source {
-    #   rules_string = <<-EOF
-    #     pass tcp from any to any port 80
-    #     pass tcp from any to any port 443
-    #     pass tcp from 213.0.113.3 to any port 22
-    #     drop tcp from any to any port 53
-    #     drop tcp from any to any port 25
-    #     drop tcp from any to any port 22
-    #   EOF         
+    rules_source {      
       stateless_rules_and_custom_actions {
         stateless_rule {
           priority = 1
@@ -44,6 +36,10 @@ resource "aws_networkfirewall_rule_group" "nwf_rule_group" {
             }     
           }
         }   
+      }
+    }
+  }
+}        
         # stateless_rule {
         #   priority = 2
         #   rule_definition {
@@ -66,10 +62,7 @@ resource "aws_networkfirewall_rule_group" "nwf_rule_group" {
         #     }     
         #   }
         # }    
-      }
-    }
-  }
-}
+
 resource "aws_networkfirewall_firewall_policy" "nwf_policy" {
   name = "nwf-policy"
   firewall_policy {
@@ -81,30 +74,30 @@ resource "aws_networkfirewall_firewall_policy" "nwf_policy" {
       resource_arn = "aws_networkfirewall_rule_group.nwf_rule_group.arn"
     }
     # 알려지고 확인된 활성 봇넷과 기타 명령 및 제어(C2) 호스트의 여러 소스에서 자동 생성된 서명
-    stateful_rule_group_reference {
-      priority     = 1        
-      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/ThreatSignaturesBotnetStrictOrder"
-    }
-    # HTTP 봇넷을 탐지하는 서명
-    stateful_rule_group_reference {
-      priority     = 2          
-      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/ThreatSignaturesBotnetWebStrictOrder"
-    }
-    # 코인 채굴을 수행하는 악성 코드를 탐지하는 규칙이 포함된 서명
-    stateful_rule_group_reference {
-      priority     = 3          
-      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/ThreatSignaturesMalwareCoinminingStrictOrder"
-    }         
-    # 합법적이지만 손상되어 멜웨어를 호스팅을 할 수 있는 도메인 클래스에 대한 차단
-    stateful_rule_group_reference {
-      priority     = 4            
-      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/AbusedLegitMalwareDomainsStrictOrder"
-    }    
-    # 봇넷을 호스팅하는 것으로 알려진 도메인에 대한 요청을 차단
-    stateful_rule_group_reference {
-      priority     = 5      
-      resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/BotNetCommandAndControlDomainsStrictOrder"
-    }    
+    # stateful_rule_group_reference {
+    #   priority     = 1        
+    #   resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/ThreatSignaturesBotnetStrictOrder"
+    # }
+    # # HTTP 봇넷을 탐지하는 서명
+    # stateful_rule_group_reference {
+    #   priority     = 2          
+    #   resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/ThreatSignaturesBotnetWebStrictOrder"
+    # }
+    # # 코인 채굴을 수행하는 악성 코드를 탐지하는 규칙이 포함된 서명
+    # stateful_rule_group_reference {
+    #   priority     = 3          
+    #   resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/ThreatSignaturesMalwareCoinminingStrictOrder"
+    # }         
+    # # 합법적이지만 손상되어 멜웨어를 호스팅을 할 수 있는 도메인 클래스에 대한 차단
+    # stateful_rule_group_reference {
+    #   priority     = 4            
+    #   resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/AbusedLegitMalwareDomainsStrictOrder"
+    # }    
+    # # 봇넷을 호스팅하는 것으로 알려진 도메인에 대한 요청을 차단
+    # stateful_rule_group_reference {
+    #   priority     = 5      
+    #   resource_arn = "arn:aws:network-firewall:us-east-1:aws-managed:stateful-rulegroup/BotNetCommandAndControlDomainsStrictOrder"
+    # }    
   }
 }
 
