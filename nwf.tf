a6faa734ea65674c5e0868c15bc832769f09bfe8
rickymanx4 <rickymanx4@naver.com>
2024-04-26 11:37:30 +0900

    nwf # edit subnets

--- nwf.tf
+++ nwf.tf
@@ -69,7 +69,10 @@ resource "aws_networkfirewall_firewall" "user_network_firewall" {
   firewall_policy_change_protection  = false
   subnet_change_protection           = false
   subnet_mapping {
-    subnet_id = [aws_subnet.subnet_user_dmz_pub[2].id, aws_subnet.subnet_user_dmz_pub[3].id]
+    subnet_id = aws_subnet.subnet_user_dmz_pub[2].id
+  }
+  subnet_mapping {
+    subnet_id = aws_subnet.subnet_user_dmz_pub[3].id    
   }
   tags = {
     Name = "${var.name[0]}-nwf" 
