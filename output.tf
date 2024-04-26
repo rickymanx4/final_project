00ea85cb67157bcae3c434f20e50f683b7a6e51e
rickymanx4 <rickymanx4@naver.com>
2024-04-26 14:56:22 +0900

    1

--- output.tf
+++ output.tf
@@ -7,5 +7,5 @@ output "dev_dmz_nexus_dns_name" {
 }
 
 output "dev_dmz_nexus_eni_pub_ip" {
-  value = data.aws_network_interfaces.nexus_nlb_ni.id
+  value = data.aws_network_interface.nexus_nlb_ni.id
 }
