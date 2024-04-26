669ad05acbbd31a8b4baabd56486ee5c0e363bdf
rickymanx4 <152831608+rickymanx4@users.noreply.github.com>
2024-04-26 16:57:15 +0900

    ee

--- output.tf
+++ output.tf
@@ -6,6 +6,6 @@ output "dev_dmz_nexus_dns_name" {
   value = aws_lb.dev_dmz_nexus_lb.dns_name
 }
 
-output "dev_dmz_nexus_eni_pub_ip" {
-  value = [for eni in data.aws_network_interfaces.nexus_nlb_ni.ids : aws_network_interface.eni[eni].association ? aws_network_interface.eni[eni].association.public_ip : null]
-}
+# output "dev_dmz_nexus_eni_pub_ip" {
+#   value = [for eni in data.aws_network_interfaces.nexus_nlb_ni.ids : aws_network_interface.eni[eni].association ? aws_network_interface.eni[eni].association.public_ip : null]
+# }
