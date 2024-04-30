d4f2a911679051d239e9cd465d83c1a933bb0eea
rickymanx4 <152831608+rickymanx4@users.noreply.github.com>
2024-04-30 12:13:18 +0900

    data # edit endp

--- data.tf
+++ data.tf
@@ -126,24 +126,24 @@ data "aws_wafv2_web_acl" "cf_wacl" {
 }
 data "aws_vpc_endpoint" "user_nwf_endpoints" {
   count =2
-  # filter {
-  #   name   = "description"
-  #   values = ["ELB net/dev-dmz-nexus-lb/*"]
-  # }
   filter {
-    name   = "subnet-id"
+    name   = "vpc_id"
+    values = [aws_vpc.project_vpc[0]]
+  }
+  filter {
+    name   = "subnet_ids"
     values = [aws_subnet.subnet_user_dmz_pub[count.index + 2].id]
   }
 }  
 
 data "aws_vpc_endpoint" "dev_nwf_endpoints" {
   count =2
-  # filter {
-  #   name   = "description"
-  #   values = ["ELB net/dev-dmz-nexus-lb/*"]
-  # }
   filter {
-    name   = "subnet-id"
+    name   = "vpc_id"
+    values = [aws_vpc.project_vpc[1]]
+  }
+  filter {
+    name   = "subnet_ids"
     values = [aws_subnet.subnet_dev_dmz_pub[count.index + 2].id]
   }
 }  
