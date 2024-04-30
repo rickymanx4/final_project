43c2da4040d614110dae98ab64696e4b2702a6dc
rickymanx4 <152831608+rickymanx4@users.noreply.github.com>
2024-04-30 12:26:41 +0900

    data # edit endpoint

--- data.tf
+++ data.tf
@@ -129,8 +129,7 @@ data "aws_vpc_endpoint" "user_nwf_endpoints" {
   vpc_id   = aws_vpc.project_vpc[0].id
  
   filter {
-    name   = "cidr-blocks"
-    values = [aws_subnet.subnet_user_dmz_pub[count.index + 2].cidr_block]
+    cidr-blocks = aws_subnet.subnet_user_dmz_pub[count.index + 2].cidr_block
   }
 }  
 
