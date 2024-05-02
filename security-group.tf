6b982bc157282ea9ebda9a577c700c84e014342a
rickymanx4 <152831608+rickymanx4@users.noreply.github.com>
2024-05-02 17:44:25 +0900

    sg

--- security-group.tf
+++ security-group.tf
@@ -66,8 +66,8 @@ resource "aws_security_group_rule" "dev_dmz_lb_SG" {
   security_group_id        = aws_security_group.dmz_lb_sg[1].id
   type                     = "ingress"
   protocol                 = "tcp"
-  from_port                = local.dmz_ports[count.index +1]
-  to_port                  = local.dmz_ports[count.index +1]
+  from_port                = local.dmz_ports[count.index + 1 ]
+  to_port                  = local.dmz_ports[count.index + 1 ]
   cidr_blocks              = ["0.0.0.0/0"]
 }
 
